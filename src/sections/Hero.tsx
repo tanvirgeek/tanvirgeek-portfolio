6c21604134c9f2cee2670945bb6a8961564c4398
Tanvirgeek <tanvirgeek@gmail.com>
2025-01-24 05:46:45 +0600

    noise

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,10 +1,17 @@
 import Image from "next/image";
 import memojiImage from "@/assets/images/memoji-computer.png";
 import ArrowDown from "@/assets/icons/arrow-down.svg";
+import grainImage from "@/assets/images/grain.jpg";
 
 export const HeroSection = () => {
   return (
-    <div className="py-32">
+    <div className="py-32 md:py-48 lg:py-60 relative z-0">
+      <div
+        className="absolute inset-0 -z-30 opacity-10"
+        style={{
+          backgroundImage: `url(${grainImage.src})`,
+        }}
+      ></div>
       <div className="container">
         <div className="flex flex-col items-center">
           <Image
@@ -19,15 +26,18 @@ export const HeroSection = () => {
             </div>
           </div>
         </div>
+        <div className="max-w-lg mx-auto">
+          <h1 className="font-serif text-center mt-8 tracking-wide md:text-5xl">
+            Building exceptional user experience
+          </h1>
+          <p className="mt-4 text-center text-white/60 md:text-lg">
+            I specilize in transforming designss into functional,
+            high-performing web applications. Let&apos;s discuss your next
+            project.
+          </p>
+        </div>
 
-        <h1 className="font-serif text-center mt-8 tracking-wide">
-          Building exceptional user experience
-        </h1>
-        <p className="mt-4 text-center text-white/60">
-          I specilize in transforming designss into functional, high-performing
-          web applications. Let&apos;s discuss your next project.
-        </p>
-        <div className="flex flex-col items-center mt-8 gap-4">
+        <div className="flex flex-col md:flex-row justify-center items-center mt-8 gap-4">
           <button className="inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl">
             <span className="font-semibold">Explore My Work</span>
             <ArrowDown className="size-4" />

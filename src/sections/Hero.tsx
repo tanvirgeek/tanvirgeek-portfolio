1cdef341d9dca3a19afb1541a86587e8307791ed
Tanvirgeek <tanvirgeek@gmail.com>
2025-01-23 08:38:04 +0600

    hero section

---
+++ src/sections/Hero.tsx
@@ -0,0 +1,44 @@
+import Image from "next/image";
+import memojiImage from "@/assets/images/memoji-computer.png";
+import ArrowDown from "@/assets/icons/arrow-down.svg";
+
+export const HeroSection = () => {
+  return (
+    <div className="py-32">
+      <div className="container">
+        <div className="flex flex-col items-center">
+          <Image
+            src={memojiImage}
+            className="size-[100px]"
+            alt="Person Peeking from behind laptop"
+          />
+          <div className="bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex items-center gap-4 rounded-lg">
+            <div className="bg-green-500 size-2.5 rounded-full"></div>
+            <div className="text-sm font-medium">
+              Available for new projects
+            </div>
+          </div>
+        </div>
+
+        <h1 className="font-serif text-center mt-8 tracking-wide">
+          Building exceptional user experience
+        </h1>
+        <p className="mt-4 text-center text-white/60">
+          I specilize in transforming designss into functional, high-performing
+          web applications. Let&apos;s discuss your next project.
+        </p>
+        <div className="flex flex-col items-center mt-8 gap-4">
+          <button className="inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl">
+            <span className="font-semibold">Explore My Work</span>
+            <ArrowDown className="size-4" />
+          </button>
+
+          <button className="inline-flex items-center gap-2 border border-white bg-white text-gray-900 h-12 px-6 rounded-xl">
+            <span>👋</span>
+            <span className="font-semibold">Let&apos;s connect</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};

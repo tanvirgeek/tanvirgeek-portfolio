7f84c7d7f481d3ba7bb363d0b1a628ef6d2fee43
Tanvirgeek <tanvirgeek@gmail.com>
2025-02-05 09:31:42 +0600

    Testimonial section

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -52,15 +52,29 @@ export const TestimonialsSection = () => {
             "Don't just take my word for it. See what my clients have to say about my work"
           }
         />
-        <div>
-          {testimonials.map((testimonial) => (
-            <Card key={testimonial.name}>
-              <Image src={testimonial.avatar} alt={testimonial.name} />
-              <div>{testimonial.name}</div>
-              <div>{testimonial.position}</div>
-              <div>{testimonial.text}</div>
-            </Card>
-          ))}
+        <div className="mt-16 flex overflow-x-clip [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
+          <div className="flex gap-8 flex-none">
+            {testimonials.map((testimonial) => (
+              <Card key={testimonial.name} className="max-w-xs">
+                <div className="flex gap-4 items-center">
+                  <div className="size-14 bg-gray-700 inline-flex rounded-full items-center justify-center">
+                    <Image
+                      src={testimonial.avatar}
+                      alt={testimonial.name}
+                      className="max-h-full"
+                    />
+                  </div>
+                  <div>
+                    <div className="font-semibold">{testimonial.name}</div>
+                    <div className="text-sm text-white/40">
+                      {testimonial.position}
+                    </div>
+                  </div>
+                </div>
+                <p className="mt-4 text-sm">{testimonial.text}</p>
+              </Card>
+            ))}
+          </div>
         </div>
       </div>
     </div>
